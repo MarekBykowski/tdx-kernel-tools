acc3c328ee4d9d5d8b3a911a2089ddcb4c12f034
Marek Bykowski <marek.bykowski@gmail.com>
2026-05-29 21:25:46 +0200

    Update run.sh
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+SUFFIX=-tdx-io-test-ready
 use_git_sha=false
 
 scripts/config --disable LOCALVERSION_AUTO
@@ -11,12 +12,12 @@ if [[ "$use_git_sha" == "true" ]]; then
 	# Does show SHA but without dirty/not dirty
 	# Typically we do not want that as this pollutes the grubby and /boot
 	SHA=$(git rev-parse --short HEAD)
-	SUFFIX="-tdx-io-g${SHA}"
+	SUFFIX="${SUFFIX}-${SHA}"
 else
 	# Only shows the string.
 	# With subsequent commits no pollution for grubby, and /boot
 	# This is what we typically want
-	SUFFIX="-tdx-io"
+	SUFFIX="${SUFFIX}"
 fi
 
 fakeroot make -j10 LOCALVERSION="$SUFFIX"
@@ -28,7 +29,7 @@ fi
 
 KERNEL_RELEASE=$(cat include/config/kernel.release)
 echo "Kernel release is: $KERNEL_RELEASE"
-if [[ $f == install ]]; then
+if [[ $1 == install ]]; then
 	echo "Kernel installed in grub and populated to /boot"
 else
 	echo "Kernel only built but not installed"
